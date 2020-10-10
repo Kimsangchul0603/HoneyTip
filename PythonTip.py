1b3660f6088cb59c4a47edef6c6e80fdf11b8853
Kimsangchul0603 <high99a@gmail.com>
2020-10-10 20:30:52 +0900

    꿀팁

--- PythonTip.py
+++ PythonTip.py
@@ -9,3 +9,9 @@ x.append(4) # 리스트에 항목 추가, x = [1, 2, 3, 4]
 
 # 딕셔너리
 dict.get("없는key", "value") # 딕셔너리 내 없는 키의 값을 value로 대체해줌
+dict.keys() # key 호출
+dict.values() # value 호출
+dict.items() # key, value를 튜플로 묶어 리스트로 호출
+
+# defaultdict : 키가 없으면 자동으로 추가해주는 딕셔너리
+from collections import defaultdict
